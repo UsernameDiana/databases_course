a9e135b5ab8118b226a6249181a31f3aa756ac25
UsernameDiana <ms.strele@gmail.com>
2018-02-06 17:36:26 +0100

    encoding

--- 01/homework_01/database.py
+++ 01/homework_01/database.py
@@ -6,13 +6,22 @@ first_arg = sys.argv[1]
 second_arg = sys.argv[2]
 
 
-def write_to(word1=first_arg, word2=second_arg):
-    file_db.write("{} {}".format(word1, word2))
+def write_to(key=first_arg, val=second_arg):
+    file_db.write("{} {}".format(key, val))
+    pair = "%s:%s" % (key, val)
+    f = file_db
+    print(pair)
+    f.write(enc(pair) + '\n')
+    f.close()
 
 
 with open('db.txt', "rb") as binary_file:
     data = binary_file.read()
     print(data)
+
+
+def enc(string):
+    return "".join([format(ord(char), '#010b')[2:] for char in string])
 
 
 if __name__ == "__main__":
